b2f077bae2129e0b2784fcbc164bf89e597cfd06
Kwag De Gi <code@skyaperture.com>
2015-01-09 15:20:04 +0900

    body-parser added

--- 0109/0109a.js
+++ 0109/0109a.js
@@ -1,14 +1,4 @@
 var http = require('http');
-
-var server = http.createServer(function(req, res) {
-	res.writeHead(200, {'Content-Type': 'text/html'});
-	res.end('Hi World\n');
-});
-
-server.listen(1337, '127.0.0.1');
-
-console.log('Server runs successfully');
-
 var express = require('express');
 var bodyParser = require('body-parser');
 var app = express();
