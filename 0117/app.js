5bf71e0adbd717e5c3a894ae20fa2de19f0c01d3
Kwag De Gi <code@skyaperture.com>
2015-01-17 03:35:57 +0900

    static ready

--- 0117/app.js
+++ 0117/app.js
@@ -1,10 +1,14 @@
 var
 express = require('express'),
 app     = express(),
-http    = require('http').Server(app);
+http    = require('http').Server(app),
+path    = require('path');
+
+app.use(express.static(path.join( path.dirname(), './www/')));
 
 app.get('/', function(req, res){
 	res.send('wow! such server! very listening!');
+	res.sendFile('index.html');
 });
 
 http.listen(4040, function(){
